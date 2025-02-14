ea512f9dbce94d187f08ca18c84c9272de9bc825
Christoffer Gylin <christoffer@christoffergylin.com>
2025-02-14 22:48:26 +0100

    Created AvailibleLanguages type and setLanguageCookie server action

--- app/actions.ts
+++ app/actions.ts
@@ -1,8 +1,14 @@
 "use server"
 
+import { type AvailibleLanguages } from "@/types/language"
 import { cookies } from "next/headers"
 
 export async function setConsentCookie() {
     const cookieStore = await cookies()
     cookieStore.set('consent', 'true')
+} 
+
+export async function setLanguageCookie(newLang: AvailibleLanguages) {
+    const cookieStore = await cookies()
+    cookieStore.set('language', newLang)
 } 
---
+++ types/language.ts
@@ -0,0 +1,1 @@
+export type AvailibleLanguages = 'en' | 'sv'
