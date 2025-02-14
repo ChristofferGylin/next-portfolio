71d94b01b8a7d3b064502028b1ade91cc1798d60
Christoffer Gylin <christoffer@christoffergylin.com>
2025-02-14 23:44:25 +0100

    Changed border color for header and footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,7 @@
 const Footer = () => {
 
     return (
-        <footer className="flex justify-between p-2 h-full bg-slate-700 border-t border-slate-400">
+        <footer className="flex justify-between p-2 h-full bg-slate-700 border-t border-slate-400/50">
             <ul className="flex justify-between gap-2">
                 <li><a href="https://www.linkedin.com/in/christoffer-gylin">Li</a></li>
                 <li><a href="https://github.com/ChristofferGylin">GH</a></li>
