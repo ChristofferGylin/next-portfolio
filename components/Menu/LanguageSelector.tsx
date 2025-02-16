081461c8cc8efa36b6bea19ab604c89e3415f532
Christoffer Gylin <christoffer@christoffergylin.com>
2025-02-16 03:55:07 +0100

    Adjusted grid layout and positioning of menu items

--- components/Menu/LanguageSelector.tsx
+++ components/Menu/LanguageSelector.tsx
@@ -21,13 +21,13 @@ const LanguageSelector = () => {
     }
 
     return (
-        <div className="flex flex-col items-center gap-0.5">
-            <div className="flex justify-between w-[54px] h-[20px]">
+        <div className="menu-grid-rows">
+            <div className="flex justify-between w-[54px] h-[20px] row-start-2 justify-self-center self-end">
                 <ImageButton src={sweFlag} alt='Change language to Swedish' callback={() => {onSetLanguage('sv')}} />
                 <p className="flex justify-center items-center">/</p>
                 <ImageButton src={ukFlag} alt='Change language to English' callback={() => {onSetLanguage('en')}} />
             </div>
-            <div className={`flex w-[60px]`}>
+            <div className={`flex w-[60px] row-start-3 justify-self-center self-center`}>
                 <Indicator width="w-[26px]" style={languageIndicatorPosition} />
             </div>
         </div>
--- components/Menu/MenuItem.tsx
+++ components/Menu/MenuItem.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link"
+import Indicator from "../UI/Indicator";
 
 type MenuItemProps = {
     href: string;
@@ -8,7 +9,12 @@ type MenuItemProps = {
 
 const MenuItem = ({ href, title, highlighted = false }: MenuItemProps) => {
 
-    return <li className={`${highlighted ? 'text-cyan-300' : ''} text-lg transition-all duration-300 ease-in-out`}><Link href={href}>{title}</Link></li>
+    return (
+        <div className="menu-grid-rows">
+            <li className={`${highlighted ? 'text-cyan-300' : ''} text-lg transition-all duration-300 h-[21px] ease-in-out row-start-2 justify-self-center self-start`}><Link href={href}>{title}</Link></li>
+            {highlighted && <Indicator width="w-full" style="row-start-3 justify-self-center self-center" />}
+        </div>
+    )
 }
 
 export default MenuItem
