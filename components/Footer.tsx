96ab62f5e07ba68344184f335279aeb71735697e
Christoffer Gylin <christoffer@christoffergylin.com>
2025-02-17 23:28:37 +0100

    Fixed CookieConsent z-index bug

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,7 @@
 const Footer = () => {
 
     return (
-        <footer className="flex justify-between p-2 h-full border-t foreground">
+        <footer className="z-30 flex justify-between p-2 h-full border-t foreground">
             <ul className="flex justify-between gap-2">
                 <li><a href="https://www.linkedin.com/in/christoffer-gylin">Li</a></li>
                 <li><a href="https://github.com/ChristofferGylin">GH</a></li>
