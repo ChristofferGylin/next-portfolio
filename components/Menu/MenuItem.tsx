550794003ecf6c43919bd489ad574aeb62ea1eda
Christoffer Gylin <christoffer@christoffergylin.com>
2025-02-17 01:07:45 +0100

    Added icon to MenuItem in mobile view, changed layout and spacing of menu items and MobileMenu

--- components/Menu/MenuItem.tsx
+++ components/Menu/MenuItem.tsx
@@ -1,16 +1,17 @@
 "use client"
 
 import Link from "next/link"
-import Indicator from "../UI/Indicator";
 import { usePathname } from "next/navigation";
+import { IconType } from "react-icons";
 
 type MenuItemProps = {
     href: string;
     title: string;
     highlighted?: boolean
+    Icon?: IconType
 }
 
-const MenuItem = ({ href, title, highlighted = false }: MenuItemProps) => {
+const MenuItem = ({ href, title, highlighted = false, Icon }: MenuItemProps) => {
 
     const pathname = usePathname()
 
@@ -19,8 +20,13 @@ const MenuItem = ({ href, title, highlighted = false }: MenuItemProps) => {
     }
 
     return (
-        <div className=" md:menu-grid-rows">
-            <li className={`${highlighted ? 'underline underline-offset-4' : 'hover:text-sky-100'}  text-lg transition-all duration-300 h-[21px] ease-in-out row-start-2 justify-self-center self-start`}><Link href={href}>{title}</Link></li>
+        <div className=" md:menu-grid-rows md:border-0 flex items-center p-2 border-b last:border-0 border-slate-400/10 w-48 md:w-fit">
+            <li className={`${highlighted ? 'underline underline-offset-4' : 'hover:text-sky-100'}  text-lg transition-all duration-300 md:h-[21px] ease-in-out row-start-2 justify-self-center self-start`}>
+                <Link href={href} className="flex gap-2 items-center">
+                    {Icon && <Icon />}
+                    {title}
+                </Link>
+            </li>
         </div>
     )
 }
--- components/Menu/MobileMenu.tsx
+++ components/Menu/MobileMenu.tsx
@@ -3,7 +3,9 @@ import LanguageSelector from "./LanguageSelector"
 import MenuItem from "./MenuItem"
 import { menuTitles } from "./menuTitles"
 import { useState } from "react"
-import { IoMenu, IoClose } from "react-icons/io5";
+import { IoMenu, IoClose, IoHome, IoInformationCircle, IoFolder } from "react-icons/io5";
+import { PiReadCvLogo } from "react-icons/pi";
+import { MdAlternateEmail } from "react-icons/md";
 import IconButton from "../UI/IconButton"
 
 const MobileMenu = () => {
@@ -21,12 +23,12 @@ const MobileMenu = () => {
         <nav className="flex md:hidden gap-3">
             <LanguageSelector />
             <IconButton Icon={visible ? IoClose : IoMenu} size="text-2xl" callback={toggleMenu}/>
-            <ul className={`z-50 fixed top-0 right-0 h-screen flex flex-col justify-start gap-1 foreground border-l my-[3rem] transition-all duration-300 ${!visible ? 'w-0' : 'w-48'}`}>
-                <MenuItem href="/" title={content.home} />
-                <MenuItem href="/about" title={content.about} />
-                <MenuItem href="/portfolio" title={content.portfolio} />
-                <MenuItem href="/cv" title={content.cv} />
-                <MenuItem href="/contact" title={content.contact} />
+            <ul className={`z-50 fixed top-0 right-0 flex flex-col justify-start foreground rounded-bl-lg mt-[3rem] transition-all duration-300 ease-in-out ${!visible ? 'w-0' : 'w-48 border-b border-l'}`}>
+                <MenuItem href="/" title={content.home} Icon={IoHome} />
+                <MenuItem href="/about" title={content.about} Icon={IoInformationCircle} />
+                <MenuItem href="/portfolio" title={content.portfolio} Icon={IoFolder} />
+                <MenuItem href="/cv" title={content.cv} Icon={PiReadCvLogo} />
+                <MenuItem href="/contact" title={content.contact} Icon={MdAlternateEmail} />
             </ul>
         </nav>
     )
