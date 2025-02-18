a888664b64f19d5a74919564a2dbf4d2567e24df
Christoffer Gylin <christoffer@christoffergylin.com>
2025-02-18 01:44:09 +0100

    Created placeholder GithubWidget component and implemented in Hero

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
     
   return (
     <>
-      <div className="flex flex-col h-full">
+      <div className="flex flex-col h-full w-full">
         <Hero />
       </div>
     </>
--- components/Hero/Hero.tsx
+++ components/Hero/Hero.tsx
@@ -1,6 +1,7 @@
 "use client"
 
 import { useLanguageContext } from "@/contexts/Language"
+import GithubWidget from "./GithubWidget"
 
 const texts = {
     en: {
@@ -19,13 +20,14 @@ const Hero = () => {
     
         const content = language === 'en' ? texts.en : texts.sv
     return (
-        <section className="flex">
+        <section className="w-full grid grid-cols-2 gap-32 p-24">
             <div className="flex flex-col gap-2">
                 <p>{content.greeting}</p>
                 <h1>Christoffer Gylin</h1>
                 <p className="text-4xl font-bold">{content.tagline}</p>
                 <p className="text-lg">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Molestiae molestias voluptatum repellendus quod delectus tenetur deserunt, iste explicabo aliquam quasi voluptatem dolorem minima impedit sint quo rerum possimus eveniet mollitia magnam eligendi. Dolore commodi harum ea suscipit, nulla est repudiandae blanditiis maiores doloribus quod quia in quasi repellendus minus, pariatur quae obcaecati illo eligendi deserunt molestiae aperiam rem ab! Consectetur vero, eligendi laborum tempore ipsum mollitia animi quibusdam facilis corrupti! Aperiam quidem repellat vero incidunt molestiae, optio deleniti autem doloremque ipsa saepe laboriosam nostrum provident. Atque, facere magnam minima quod autem velit ipsa harum non deserunt maxime, numquam sit sint?</p>
             </div>
+            <GithubWidget />
         </section>
     )
 }
