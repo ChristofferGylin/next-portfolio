b0311eb30c423fdcb64cc49ab08cc529c721324b
Christoffer Gylin <christoffer@christoffergylin.com>
2025-02-18 04:45:12 +0100

    created aboutTexts for about component, imported and implemented in Hero

--- components/Hero/Hero.tsx
+++ components/Hero/Hero.tsx
@@ -2,6 +2,7 @@
 
 import { useLanguageContext } from "@/contexts/Language"
 import GithubWidget from "./GithubWidget"
+import { aboutTexts } from "../About/aboutTexts"
 
 const texts = {
     en: {
@@ -19,13 +20,17 @@ const Hero = () => {
     const { language } = useLanguageContext()
     
         const content = language === 'en' ? texts.en : texts.sv
+        const bodyText = language === 'en' ? aboutTexts.en.bodyText : aboutTexts.sv.bodyText
     return (
         <section className="w-full grid grid-cols-2 gap-32 p-24">
-            <div className="flex flex-col gap-2">
+            <div className="aspect-square flex flex-col gap-6 overflow-hidden">
                 <p>{content.greeting}</p>
                 <h1>Christoffer Gylin</h1>
                 <p className="text-4xl font-bold">{content.tagline}</p>
-                <p className="text-lg">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Molestiae molestias voluptatum repellendus quod delectus tenetur deserunt, iste explicabo aliquam quasi voluptatem dolorem minima impedit sint quo rerum possimus eveniet mollitia magnam eligendi. Dolore commodi harum ea suscipit, nulla est repudiandae blanditiis maiores doloribus quod quia in quasi repellendus minus, pariatur quae obcaecati illo eligendi deserunt molestiae aperiam rem ab! Consectetur vero, eligendi laborum tempore ipsum mollitia animi quibusdam facilis corrupti! Aperiam quidem repellat vero incidunt molestiae, optio deleniti autem doloremque ipsa saepe laboriosam nostrum provident. Atque, facere magnam minima quod autem velit ipsa harum non deserunt maxime, numquam sit sint?</p>
+                <div className="flex flex-col gap-6 overflow-hidden text-xl">
+                    {<p>{bodyText[0]}</p>}
+                    {<p className="line-clamp-5">{bodyText[1]}..</p>}
+                </div>
             </div>
             <GithubWidget />
         </section>
