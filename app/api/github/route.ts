880483e91615e00e6e943bca639d24073abc7933
Christoffer Gylin <christoffer@christoffergylin.com>
2025-11-03 20:07:43 +0100

    Changed github route to fetch 100 repos and sort on last pushed

--- app/api/github/route.ts
+++ app/api/github/route.ts
@@ -7,43 +7,27 @@ const octokit = new Octokit({
 
 export async function GET() {
 
-    try {
-        const repos = await octokit.request("GET /user/repos")
 
-        const reposWithLanguages = await batchFetch(repos.data, 8, async (repo) => {
+    try {
+        const repos = await octokit.request("GET /user/repos", {
+            per_page: 100,
+            visibility: "all",
+            sort: "pushed",
+            direction: "desc",
+            
+        })
+
+        const languages = await batchFetch(repos.data, 8, async (repo) => {
         try {
             const res = await octokit.request(`GET ${repo.languages_url}`);
-            return { ...repo, languages: res.data };
+            return { languages: res.data };
         } catch (err) {
             console.warn(`Failed to fetch languages for ${repo.full_name}`);
-            return { ...repo, languages: {} };
+            return
         }
         })
 
-
-        console.log('*************************************')
-        console.log('*************************************')
-        console.log('*************************************')
-        
-        console.log('number of repos:', repos.data.length)
-
-        for (const repo of repos.data) {
-
-            console.log(repo.name)
-
-        }
-        
-
-        console.log('number of repos with language:', reposWithLanguages.length)
-
-        for (const withLanguage of reposWithLanguages) {
-
-            console.log(withLanguage.name)
-            console.log(withLanguage.languages)
-
-        }
-
-        return Response.json(repos) 
+        return Response.json(languages) 
     } catch {
         console.log('Error')
         return Response.json({error: 'Error'})
