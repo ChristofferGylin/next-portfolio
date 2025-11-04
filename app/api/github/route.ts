ec0e70ffe38e3d0dc87cc4d16ad4f7c368c41457
Christoffer Gylin <christoffer@christoffergylin.com>
2025-11-04 13:26:45 +0100

    Chnaged github route to calculate and sort, route now returns a sorted array of percentages for each language

--- app/api/github/route.ts
+++ app/api/github/route.ts
@@ -10,24 +10,55 @@ export async function GET() {
 
     try {
         const repos = await octokit.request("GET /user/repos", {
-            per_page: 100,
+            per_page: 20,
             visibility: "all",
             sort: "pushed",
             direction: "desc",
             
         })
 
-        const languages = await batchFetch(repos.data, 8, async (repo) => {
-        try {
-            const res = await octokit.request(`GET ${repo.languages_url}`);
-            return { languages: res.data };
-        } catch (err) {
-            console.warn(`Failed to fetch languages for ${repo.full_name}`);
-            return
-        }
+        const languagesFromRepos = await batchFetch(repos.data, 8, async (repo) => {
+            try {
+                const res = await octokit.request(`GET ${repo.languages_url}`);
+                return { languages: res.data };
+            } catch (err) {
+                console.warn(`Failed to fetch languages for ${repo.full_name}`);
+                return
+            }
         })
 
-        return Response.json(languages) 
+        const languagesCalculated: Record<string, number> = {}
+        
+        for (const repo of languagesFromRepos) {
+            for (const key in repo?.languages) {
+                if (languagesCalculated[key]) {
+                    languagesCalculated[key] = languagesCalculated[key] + repo.languages[key]
+                } else {
+                    languagesCalculated[key] = repo.languages[key]
+                }
+            }
+        }
+
+        const languagesSorted = Object.entries(languagesCalculated).sort((a, b) => b[1] - a[1])
+
+        let totalBytes = languagesSorted.reduce((sum, [, bytes]) => sum + bytes, 0)
+        let other = 0
+
+        const languagePercentages: [string, number][] = []
+
+        for (const lang of languagesSorted) {
+            const percent = lang[1] / totalBytes * 100
+
+            if (percent < 1) {
+                other += percent
+            } else {
+                languagePercentages.push([lang[0], lang[1] / totalBytes * 100])
+            } 
+        }
+
+        languagePercentages.push(['Other', other])
+
+        return Response.json(languagePercentages) 
     } catch {
         console.log('Error')
         return Response.json({error: 'Error'})
