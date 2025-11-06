b9bb353e09e21eb6ebefe4b317ef8e03a5f73132
Christoffer Gylin <christoffer@christoffergylin.com>
2025-11-06 16:40:56 +0100

    Changed githubv route handler to modern syntax and added basic error handling

--- app/api/github/route.ts
+++ app/api/github/route.ts
@@ -1,60 +1,62 @@
 import batchFetch from "@/utils/batchFetch";
+import { NextApiRequest, NextApiResponse } from "next";
 import { Octokit } from "octokit";
 
 const octokit = new Octokit({
     auth: process.env.GITHUB_TOKEN
 })
 
-export async function GET() {
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+    if (req.method === 'GET') {
+        try {
+            const repos = await octokit.request("GET /user/repos", {
+                per_page: 20,
+                visibility: "all",
+                sort: "pushed",
+                direction: "desc",
+            })
 
+            const languagesFromRepos = await batchFetch(repos.data, 8, async (repo) => {
+                try {
+                    const res = await octokit.request(`GET ${repo.languages_url}`);
+                    return { languages: res.data };
+                } catch (err) {
+                    console.warn(`Failed to fetch languages for ${repo.full_name}`);
+                    console.error(err)
+                    return
+                }
+            })
 
-    try {
-        const repos = await octokit.request("GET /user/repos", {
-            per_page: 20,
-            visibility: "all",
-            sort: "pushed",
-            direction: "desc",
-            
-        })
-
-        const languagesFromRepos = await batchFetch(repos.data, 8, async (repo) => {
-            try {
-                const res = await octokit.request(`GET ${repo.languages_url}`);
-                return { languages: res.data };
-            } catch (err) {
-                console.warn(`Failed to fetch languages for ${repo.full_name}`);
-                console.error(err)
-                return
-            }
-        })
-
-        const languagesCalculated: Record<string, number> = {}
+            const languagesCalculated: Record<string, number> = {}
         
-        for (const repo of languagesFromRepos) {
-            for (const key in repo?.languages) {
-                if (languagesCalculated[key]) {
-                    languagesCalculated[key] = languagesCalculated[key] + repo.languages[key]
-                } else {
-                    languagesCalculated[key] = repo.languages[key]
+            for (const repo of languagesFromRepos) {
+                for (const key in repo?.languages) {
+                    if (languagesCalculated[key]) {
+                        languagesCalculated[key] = languagesCalculated[key] + repo.languages[key]
+                    } else {
+                        languagesCalculated[key] = repo.languages[key]
+                    }
                 }
             }
+
+            const totalBytes = Object.values(languagesCalculated).reduce((acc, val) => acc + val, 0);
+
+            const languagePercentages: Record<string, number> = {}
+
+            for (const lang in languagesCalculated) {
+                const percent = languagesCalculated[lang] / totalBytes * 100
+
+                if (percent > 1) {
+                    languagePercentages[lang] = percent
+                } 
+            }
+
+        
+        
+            return res.status(200).json(languagePercentages) 
+        } catch (error) {
+            console.error(error) 
+            return res.status(500).json({ error })
         }
-
-        const totalBytes = Object.values(languagesCalculated).reduce((acc, val) => acc + val, 0);
-
-        const languagePercentages: Record<string, number> = {}
-
-        for (const lang in languagesCalculated) {
-            const percent = languagesCalculated[lang] / totalBytes * 100
-
-            if (percent > 1) {
-                languagePercentages[lang] = percent
-            } 
-        }
-
-        return Response.json(languagePercentages) 
-    } catch {
-        console.log('Error')
-        return Response.json({error: 'Error'})
     }
 }
