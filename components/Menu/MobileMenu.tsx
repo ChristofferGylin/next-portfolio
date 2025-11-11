2ad06ea0096214f970ee54a4c911b342dad97f60
Christoffer Gylin <christoffer@christoffergylin.com>
2025-11-11 19:44:33 +0100

    Changed MenuItem and IconButton to accept ReactElement as prop instead of IconType

--- components/Menu/MobileMenu.tsx
+++ components/Menu/MobileMenu.tsx
@@ -5,7 +5,8 @@ import LanguageSelector from "./LanguageSelector"
 import MenuItem from "./MenuItem"
 import { menuTitles } from "./menuTitles"
 import { useState } from "react"
-import { IoMenu, IoClose } from "react-icons/io5";
+import { IoMenu, IoClose, IoHome, IoInformationCircle, IoFolder, IoDocumentText } from "react-icons/io5";
+import { MdAlternateEmail } from "react-icons/md";
 import IconButton from "../UI/IconButton"
 
 const MobileMenu = () => {
@@ -19,16 +20,18 @@ const MobileMenu = () => {
         setVisible((old) => !old)
     }
 
+    const iconStyle = "text-2xl text-sky-200 stroke-sky-200 group-hover/ButtonGroup:text-sky-50 group-hover/ButtonGroup:stroke-sky-50"
+
     return (
         <nav className="flex md:hidden gap-3">
             <LanguageSelector />
-            <IconButton Icon={visible ? IoClose : IoMenu} size="text-2xl" callback={toggleMenu}/>
+            <IconButton Icon={visible ? <IoClose className={iconStyle} /> : <IoMenu className={iconStyle} />} callback={toggleMenu}/>
             <ul className={`z-50 fixed top-0 right-0 flex flex-col justify-start foreground rounded-bl-lg mt-[3rem] transition-all duration-300 ease-in-out ${!visible ? 'w-0' : 'w-48 border-b border-l'}`}>
-                <MenuItem href="/" title={content.home} />
-                <MenuItem href="/about" title={content.about} />
-                <MenuItem href="/portfolio" title={content.portfolio} />
-                <MenuItem href="/cv" title={content.cv} />
-                <MenuItem href="/contact" title={content.contact} />
+                <MenuItem href="/" title={content.home} Icon={<IoHome />} />
+                <MenuItem href="/about" title={content.about} Icon={<IoInformationCircle />} />
+                <MenuItem href="/portfolio" title={content.portfolio} Icon={<IoFolder />} />
+                <MenuItem href="/cv" title={content.cv} Icon={<IoDocumentText />} />
+                <MenuItem href="/contact" title={content.contact} Icon={<MdAlternateEmail />} />
             </ul>
         </nav>
     )
--- components/UI/IconButton.tsx
+++ components/UI/IconButton.tsx
@@ -1,17 +1,15 @@
-import { IconType } from "react-icons"
+import { ReactElement } from "react";
 
 export type IconButtonProps = {
-    Icon: IconType;
-    size?: string;
-    color?: string;
+    Icon: ReactElement;
     callback: () => void;
     style?: string;
 }
 
-const IconButton = ({ Icon, size = 'text-xl', color = 'text-sky-200 stroke-sky-200 group-hover/ButtonGroup:text-sky-50 group-hover/ButtonGroup:stroke-sky-50', style = '', callback }: IconButtonProps) => {
+const IconButton = ({ Icon, style = '', callback }: IconButtonProps) => {
     return (
         <button onClick={callback} className={`group/ButtonGroup flex justify-center items-center justify-self-center self-center h-fit aspect-square ${style}`}>
-            <Icon className={`${size} ${color}`} />
+            {Icon}
         </button>
     )
 }
